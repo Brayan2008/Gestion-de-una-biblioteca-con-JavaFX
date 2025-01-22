7cd09e2af153b29f877c8f3db11bbefa0986c785
Paco <bmaster20082005@gmail.com>
2025-01-21 19:30:00 -0500

    v0.2.8

--- src/main/java/sistemas/biblioteca/controllers/principal.java
+++ src/main/java/sistemas/biblioteca/controllers/principal.java
@@ -2,22 +2,62 @@ package sistemas.biblioteca.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import sistemas.biblioteca.script.animaciones_predefinidas;
 
 public class principal extends animaciones_predefinidas {
 
+    /* Barra */
     @FXML
-    ImageView user_icon;
+    ImageView user_icon, settings_icon, register_icon, books_icon, chat_icon, home_icon;
+    
+    /*Panel principal*/
     @FXML
-    ImageView settings_icon;
+    HBox slides_info;
     @FXML
-    ImageView register_icon;
+    ImageView chat_button, books_button, weather_button;
+    
+    
+    /* Animaciones */
+
+
     @FXML
-    ImageView books_icon;
+    public void infocusanimated_booksButton() {
+        infocus(books_button);
+    }
     @FXML
-    ImageView chat_icon;
+    public void nofocusanimated_booksButton() {
+        nofocus(books_button);
+    }
+    
     @FXML
-    ImageView home_icon;
+    public void infocusanimated_weatherButton() {
+        infocus(weather_button);
+    }
+    @FXML
+    public void nofocusanimated_weatherButton() {
+        nofocus(weather_button);
+    }
+
+    @FXML
+    public void infocusanimated_slides() {
+        infocus(slides_info);
+    }
+
+    @FXML
+    public void nofocusanimated_slides() {
+        nofocus(slides_info);
+    }
+
+    @FXML
+    public void infocusanimated_chatButton() {
+        infocus(chat_button);
+    }
+
+    @FXML
+    public void nofocusanimated_chatButton() {
+        nofocus(chat_button);
+    }
 
     @FXML
     public void infocusanimated_user() {
@@ -79,9 +119,11 @@ public class principal extends animaciones_predefinidas {
         nofocus(books_icon);
     }
 
+
     @FXML
     public void switchPrestamo() {
         System.out.println("Entraste a prestamo");
     }
+    
 
 }
